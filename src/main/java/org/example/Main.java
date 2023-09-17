599b8a8a634bbe98d9acf4b45fb0e2de813c1734
Raisa <vlasenkoraisa9723@gmail.com>
2023-09-18 01:58:57 +0200

    Modul_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,39 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Опис Gradle-проекту:");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // 1. Ініціалізація проекту
+        System.out.println("1. Ініціалізація проекту:");
+        System.out.println("   - Використовувала команду 'gradle init' для створення нового проекту.");
+        System.out.println("   - Ця команда створює базову структуру каталогів та файли налаштувань.");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // 2. Конфігурація build.gradle
+        System.out.println("2. Конфігурація build.gradle:");
+        System.out.println("   - Файл 'build.gradle' містить конфігураційні налаштування для мого проекту.");
+        System.out.println("   - Там можна додати залежності, плагіни, завдання тощо.");
+
+        // 3. Додавання залежностей
+        System.out.println("3. Додавання залежностей:");
+        System.out.println("   - В секції 'dependencies' в 'build.gradle', додала залежності для мого проекту.");
+        System.out.println("   - Наприклад, для додавання бібліотеки GSON використовуйте 'implementation 'com.google.code.gson:gson:2.8.9''.");
+
+        // 4. Створення основного коду
+        System.out.println("4. Створення основного коду:");
+        System.out.println("   - Мій основний код зберігається в 'src/main/java'.");
+        System.out.println("   - Створила там пакет та класи, які мені потрібні.");
+
+        // 5. Запуск проекту
+        System.out.println("5. Запуск проекту:");
+        System.out.println("   - Використовувала команду 'gradle run' для запуску свого проекту, якщо плагін 'application'.");
+
+        // 6. Створення виконуваного JAR
+        System.out.println("6. Створення виконуваного JAR:");
+        System.out.println("   - Для створення 'fat JAR' або JAR, який містить всі залежності, налаштувала завдання 'jar' в 'build.gradle'.");
+        System.out.println("   - Після цього  створила JAR за допомогою команди 'gradle jar'.");
+
+
     }
 }
